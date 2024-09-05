80ab24414315df45da1c2e95d25a47b86803f77e
Lucas <lucasaraujobr230@gmail.com>
2024-09-05 16:36:43 -0300

    Igual?

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,28 +1,14 @@
 public class Operadores {
     public static void main(String[] args) {
-     
-     String nome1 = "lucas";
-     String nome2 = new String ("lucas");
+    boolean condicao1 = true;
+    boolean condicao2 = false;
+    
+    if (condicao1 && condicao2){
+        System.out.println("As duas são iguais");
+    }
 
-     System.out.println(nome1.equals(nome2));
-     
-        int numero1 = 1;
-     int numero2 = 2;
-
-     boolean simNao = numero1 == numero2;
-
-     if (numero1 < numero2) {
-        System.out.println("a nossa condição é verdadeira");
-     }
-     System.out.println("numero1 é iagual ao numero2? " + simNao);
-     
-     simNao = numero1 != numero2;
-     System.out.println("numero1 é iagual ao numero2? " + simNao);
-     
-     simNao = numero1 >numero2;
-     System.out.println("numero1 é iagual ao numero2? " + simNao);
-     
-     simNao = numero1 < numero2;
-     System.out.println("numero1 é iagual ao numero2? " + simNao);
+    if (condicao1 || condicao2){
+        System.out.println("Uma das duas é verdadeira");
+    }
     }
 }
