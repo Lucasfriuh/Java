3e5b8882acca2e9bd692776c1cdbbd813fb23682
Lucas <lucasaraujobr230@gmail.com>
2024-09-05 18:26:26 -0300

    mudanças q eu tentei fazer

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -1,38 +1,85 @@
 public class SmartTv {
-    static boolean ligada = false;
-    static int canal = 1;
-    static int volume = 25;
+    private boolean ligada = false;
+    private int canal = 1;
+    private int volume = 25;
+    private final int VOLUME_MAXIMO = 100;
+    private final int VOLUME_MINIMO = 0;
+    private final int CANAL_MAXIMO = 100;
+    private final int CANAL_MINIMO = 1;
 
-    public void mudarCanal (int novoCanal){
-        canal = novoCanal;
-
+    public void mudarCanal(int novoCanal) {
+        if (novoCanal >= CANAL_MINIMO && novoCanal <= CANAL_MAXIMO) {
+            canal = novoCanal;
+            System.out.println("Canal mudado para: " + canal);
+        } else {
+            System.out.println("Canal inválido.");
+        }
     }
 
-
-    public void aumentarCanal(){
-        canal ++;
-        System.out.println("aumentando canal");
-    }
-    public void diminuirCanal(){
-        canal --;
-        System.out.println("diminuindo canal");
-    }
-    public void aumentarVolume(){
-        volume ++;
-        System.out.println("aumentando o volume para: " + volume);
+    public void aumentarCanal() {
+        if (canal < CANAL_MAXIMO) {
+            canal++;
+        } else {
+            System.out.println("Já está no canal máximo.");
+        }
+        System.out.println("Canal atual: " + canal);
     }
 
-    public void diminuirVolume(){
-        volume --;
-        System.out.println("diminuindo o volume para: " + volume);
+    public void diminuirCanal() {
+        if (canal > CANAL_MINIMO) {
+            canal--;
+        } else {
+            System.out.println("Já está no canal mínimo.");
+        }
+        System.out.println("Canal atual: " + canal);
     }
 
-    public void ligar(){
-        ligada = true;
+    public void aumentarVolume() {
+        if (volume < VOLUME_MAXIMO) {
+            volume++;
+            System.out.println("Aumentando o volume para: " + volume);
+        } else {
+            System.out.println("Volume já está no máximo.");
+        }
     }
 
-    public void desligar(){
-        ligada = false;
+    public void diminuirVolume() {
+        if (volume > VOLUME_MINIMO) {
+            volume--;
+            System.out.println("Diminuindo o volume para: " + volume);
+        } else {
+            System.out.println("Volume já está no mínimo.");
+        }
     }
 
+    public void ligar() {
+        if (!ligada) {
+            ligada = true;
+            System.out.println("TV ligada.");
+        } else {
+            System.out.println("A TV já está ligada.");
+        }
+    }
+
+    public void desligar() {
+        if (ligada) {
+            ligada = false;
+            System.out.println("TV desligada.");
+        } else {
+            System.out.println("A TV já está desligada.");
+        }
+    }
+
+    // Getters para acessar o estado atual
+    public int getCanal() {
+        return canal;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public boolean isLigada() {
+        return ligada;
+    }
 }
--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,27 +1,15 @@
 public class Usuario {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SmartTv smartTv = new SmartTv();
         
-        smartTv.desligar();
-        System.out.println("Novo Status -> Tv ligada?" + SmartTv.ligada);
         smartTv.ligar();
-        System.out.println("Novo Status -> Tv ligada?" + SmartTv.ligada);
-        System.out.println("Tv ligada?" + SmartTv.ligada);
+        System.out.println("TV ligada? " + smartTv.isLigada());
         
-        smartTv.diminuirVolume();
-        smartTv.diminuirVolume();
-        smartTv.diminuirVolume();
         smartTv.aumentarVolume();
-        System.out.println("Volume atual: " +SmartTv.volume);
-
-
-        System.out.println("Canal atual: " + SmartTv.canal);
-        smartTv.mudarCanal(13);
-        System.out.println("Canal atual: " + SmartTv.canal);
-
-
-
-
+        smartTv.diminuirVolume();
+        System.out.println("Volume atual: " + smartTv.getVolume());
         
+        smartTv.mudarCanal(13);
+        System.out.println("Canal atual: " + smartTv.getCanal());
     }
 }
